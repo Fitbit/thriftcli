ff697dc9abd928c19d0a8e8a2e2b4de8fd6776d2
Neel Virdy <nvirdy@fitbit.com>
2016-07-15 14:14:12 -0700

    IPD-38974 Added nested tests for request body converter

--- tests/test_request_body_converter.py
+++ tests/test_request_body_converter.py
@@ -9,10 +9,12 @@ class TestRequestBodyConverter(unittest.TestCase):
     def test_json_request_body(self):
         self.assertEqual(data.TEST_ARGUMENT_DICTIONARY, convert(data.TEST_JSON_REQUEST_BODY))
         self.assertEqual(data.TEST_ARGUMENT_DICTIONARY2, convert(data.TEST_JSON_REQUEST_BODY2))
+        self.assertEqual(data.TEST_ARGUMENT_DICTIONARY3_ALT, convert(data.TEST_JSON_REQUEST_BODY3))
 
     def test_java_thrift_request_body(self):
         self.assertEqual(data.TEST_ARGUMENT_DICTIONARY, convert(data.TEST_JAVA_THRIFT_REQUEST_BODY))
         self.assertEqual(data.TEST_ARGUMENT_DICTIONARY2, convert(data.TEST_JAVA_THRIFT_REQUEST_BODY2))
+        self.assertEqual(data.TEST_ARGUMENT_DICTIONARY3, convert(data.TEST_JAVA_THRIFT_REQUEST_BODY3))
 
     def test_invalid_request_body(self):
         with self.assertRaises(ValueError):
